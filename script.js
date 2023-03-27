c8f0229f42b2a11540c623cd69e4279b40002287
Sarthak1918 <pandasarthak12345@gmail.com>
2023-03-27 18:47:58 +0530

    moves counter and popper effect

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ function mapTime() {
   return `${`${minutes}`.padStart(2, "0")}:${`${seconds}`.padStart(2, "0")}`;
 }
 
-
 function startCountDown() {
   if (countDownStarted && !gameEnded) {
     document.getElementById("timer").innerHTML = mapTime();
@@ -20,19 +19,51 @@ function startCountDown() {
   }
 }
 
+function shuffleCards() {
+  const parent = document.querySelector(".container");
+  const childs = parent.children;
+  for (let i = 0; i < childs.length; i++) {
+    const j = Math.floor(Math.random() * childs.length);
+    const a = childs[i];
+    const b = childs[j];
+    parent.insertBefore(b,a);  //b- to be inserted    a-above which b to be inserted
+  }
+}
+
+shuffleCards();
+
 const cards = document.querySelectorAll(".item");
 
 function resetGame() {
   countDownStarted = false;
   gameEnded = false;
   document.getElementById("timer").innerHTML = "00:00";
-  cards.forEach((e)=>{
-    e.classList.remove("clicked")
-    e.classList.remove("correct")
-    e.classList.remove("shake")
-    e.classList.remove("non-clickable")
-  })
+  document.getElementById("result").style.display = "none";
+  cards.forEach((e) => {
+    e.classList.remove("clicked");
+    e.classList.remove("correct");
+    e.classList.remove("shake");
+    e.classList.remove("non-clickable");
+  });
+  shuffleCards();
+  updateMoves(0);
 }
+
+let moves = 0;
+
+function updateMoves(index) {
+  // type your code here - 10 marks
+  document.getElementById("move").innerHTML=index
+  moves=index;
+}
+
+function addPopper() {
+  document.getElementById("popper").innerHTML = `<lottie-player src="https://assets8.lottiefiles.com/packages/lf20_C51Bca6c0m.json" background="transparent" speed="2" style="width: 100vw; height: 100vh;" autoplay></lottie-player>`;
+  setTimeout(() => {
+    document.getElementById("popper").innerHTML = "";
+  }, 4000);
+}
+
 
 cards.forEach((card) => {
   card.addEventListener("click", function () {
@@ -47,9 +78,12 @@ cards.forEach((card) => {
     }
     if (count === 0) {
       firstSelection = card.getAttribute("animal");
-      card.classList.add("non-clickable")
+      card.classList.add("non-clickable");
       count++;
     } else {
+
+      updateMoves(moves + 1);
+
       secondSelection = card.getAttribute("animal");
       count = 0;
 
@@ -63,6 +97,7 @@ cards.forEach((card) => {
         correctCards[0].classList.add("non-clickable");
         correctCards[1].classList.add("non-clickable");
       } else {
+
         let WrongCards = document.querySelectorAll(".clicked");
         WrongCards[0].classList.remove("non-clickable");
         WrongCards[1].classList.remove("non-clickable");
@@ -87,11 +122,9 @@ cards.forEach((card) => {
       });
       if (correctCount === cards.length) {
         gameEnded = true;
-        document.getElementById("result").style.display=""
+        document.getElementById("result").style.display = "";
+        addPopper();
       }
     }
   });
-
-  
-
 });
