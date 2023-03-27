cf9b9999cced70dcbf4683f9ef026e95a1a8a991
Sarthak1918 <pandasarthak12345@gmail.com>
2023-03-27 18:55:48 +0530

    update script.js
    update the animation time of the popper

--- script.js
+++ script.js
@@ -58,10 +58,10 @@ function updateMoves(index) {
 }
 
 function addPopper() {
-  document.getElementById("popper").innerHTML = `<lottie-player src="https://assets8.lottiefiles.com/packages/lf20_C51Bca6c0m.json" background="transparent" speed="2" style="width: 100vw; height: 100vh;" autoplay></lottie-player>`;
+  document.getElementById("popper").innerHTML = `<lottie-player src="https://assets8.lottiefiles.com/packages/lf20_C51Bca6c0m.json" background="transparent" speed="1" style="width: 100vw; height: 100vh;" autoplay></lottie-player>`;
   setTimeout(() => {
     document.getElementById("popper").innerHTML = "";
-  }, 4000);
+  }, 5000);
 }
 
 
